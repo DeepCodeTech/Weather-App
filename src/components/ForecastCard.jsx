0a57da168b41a1e0d6ecc6f4faea4da96f78b807
Deep Singh <77362196+DeepCodeTech@users.noreply.github.com>
2025-02-20 02:56:52 +0530

    Update ForecastCard.jsx

--- src/components/ForecastCard.jsx
+++ src/components/ForecastCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ForecastCard = ({ day, highTemp, lowTemp, iconUrl }) => {
   return (
-    <div className="forecast-card">
+    <div className="forecast-card rounded-[20px] w-[200px] padding-block-[20px] shadow-[0px_4px_24px_rgba(0,0,0,0.20)]">
       <h3>{day}</h3>
       <img src={iconUrl} alt="weather icon" className="m-auto" />
       <p>High: {highTemp}</p>
